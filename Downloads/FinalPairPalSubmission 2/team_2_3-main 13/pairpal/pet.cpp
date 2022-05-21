b9c68a5a8d8d794cf591d32089802483996b235a
shenlu <shenlu@lafayette.edu>
2022-05-21 11:53:04 -0400

    Pet Matching Project

--- Downloads/FinalPairPalSubmission 2/team_2_3-main 13/pairpal/pet.cpp
+++ Downloads/FinalPairPalSubmission 2/team_2_3-main 13/pairpal/pet.cpp
@@ -25,32 +25,34 @@ void pet::updateAge(int newAge)
     age = newAge;
 }
 
-void pet::updateHealth(std::string typeOfCondition,std::string nameOfCondition)
-{
-    if(listOfConditions.contains(nameOfCondition)) {}//then don't duplicate pair
-    else{
-    //check what type of condition the pet has
-    if(typeOfCondition == "disease")
-    {value = value + 2;}
+//void pet::updateHealth(std::string typeOfCondition,std::string nameOfCondition)
+//{
+//    if(listOfConditions.contains(nameOfCondition)) {}//then don't duplicate pair
+//    else{
+//    //check what type of condition the pet has
+//    if(typeOfCondition == "disease")
+//    {value = value + 2;}
 
-    else if (typeOfCondition == "allergy")
-    {value = value + 1;}
+//    else if (typeOfCondition == "allergy")
+//    {value = value + 1;}
 
-    else if (typeOfCondition == "minor condition")
-    {value = value + 1;}
+//    else if (typeOfCondition == "minor condition")
+//    {value = value + 1;}
 
-    else if (typeOfCondition == "mental illness")
-    {value = value + 3;}
+//    else if (typeOfCondition == "mental illness")
+//    {value = value + 3;}
 
-    else if (typeOfCondition == "terminal")
-    {value = value + 4;}
+//    else if (typeOfCondition == "terminal")
+//    {value = value + 4;}
 
-    else value = value + 0;
-    listOfConditions.push_back(typeOfCondition, nameOfCondition);
-}
+//    else value = value + 0;
+//    listOfConditions.push_back(typeOfCondition, nameOfCondition);
+//}
     //add condition
     // if it already exists in the arrayList/Deque then don't duplicate
-}
+//}
+
+
 //! A normal member that does not take any argument and does not return anything,
 //! and it will display the information.
 std::string pet::displayInfo()
